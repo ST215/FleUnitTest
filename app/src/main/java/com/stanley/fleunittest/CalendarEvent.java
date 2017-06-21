e56519f2fa1e41bcde9dd01c451879b3d6a04d53
Stanley H Griggs II <shg5004@gmail.com>
2017-06-20 22:09:32 -0400

    Calendar Event Data Model Model
    Added Calendar Event Data Model

---
+++ app/src/main/java/com/stanley/fleunittest/CalendarEvent.java
@@ -0,0 +1,12 @@
+package com.stanley.fleunittest;
+
+/**
+ * Created by Stanley on 6/20/17.
+ */
+
+public class CalendarEvent {
+
+    String eventName;
+    long eventStart;
+    long eventEnd;
+}
