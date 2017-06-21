c79f59c42bd8415206a8b4e7745038bd9f2a9a4f
Stanley H Griggs II <shg5004@gmail.com>
2017-06-20 22:47:27 -0400

    Generated CalendarEvent Constructor
    Added Constructer for Calendar Event using Generate

--- app/src/main/java/com/stanley/fleunittest/CalendarEvent.java
+++ app/src/main/java/com/stanley/fleunittest/CalendarEvent.java
@@ -9,4 +9,10 @@ public class CalendarEvent {
     String eventName;
     long eventStart;
     long eventEnd;
+
+    public CalendarEvent(String eventName, long eventStart, long eventEnd) {
+        this.eventName = eventName;
+        this.eventStart = eventStart;
+        this.eventEnd = eventEnd;
+    }
 }
