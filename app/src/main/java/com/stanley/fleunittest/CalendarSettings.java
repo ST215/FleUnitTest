42de5bb9f9d1553da4dda18c0a002bb13b4d2494
Stanley H Griggs II <shg5004@gmail.com>
2017-06-21 01:58:14 -0400

    Generated Calendar Settings Constructor

--- app/src/main/java/com/stanley/fleunittest/CalendarSettings.java
+++ app/src/main/java/com/stanley/fleunittest/CalendarSettings.java
@@ -9,4 +9,10 @@ public class CalendarSettings {
     long workDayStart;
     long workDayEnd;
     long workDayMidday;
+
+    public CalendarSettings(long workDayStart, long workDayEnd, long workDayMidday) {
+        this.workDayStart = workDayStart;
+        this.workDayEnd = workDayEnd;
+        this.workDayMidday = workDayMidday;
+    }
 }
