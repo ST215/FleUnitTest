0a91249c742d061a82660407eeac7d1abcd2fd78
Stanley H Griggs II <shg5004@gmail.com>
2017-06-20 22:10:34 -0400

    Calendar Settings Data Model
    Calendar Settings Data Model Model

---
+++ app/src/main/java/com/stanley/fleunittest/CalendarSettings.java
@@ -0,0 +1,12 @@
+package com.stanley.fleunittest;
+
+/**
+ * Created by Stanley on 6/20/17.
+ */
+
+public class CalendarSettings {
+
+    long workDayStart;
+    long workDayEnd;
+    long workDayMidday;
+}
